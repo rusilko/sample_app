ba5bf3d45eb52319bcc9d2a9cf356f212db3e501
Tomek Rusilko <tomek.rusilko@gmail.com>
2010-09-08 18:39:22 +0200

    Done with user singin

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -118,6 +118,12 @@ describe UsersController do
         response.should redirect_to(user_path(assigns(:user)))
       end
       
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
+      
+      
     end
   end
 end
